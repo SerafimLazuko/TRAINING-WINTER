7da77ffcb7e6f962def1365f0886eaaa7e6e42a0
serafim <serafim.lazuko@gmail.com>
2018-10-17 01:09:06 +0300

    add GCDFinder, without XML-comments and Time

---
+++ NET.W.2018.Lazuko.03-04/GCDEuclideanAlgorithm/GCDEuclideanAlgorithm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDEuclideanAlgorithm
+{
+    public static class GCDFinder
+    {
+
+        #region Public Euclidean Algorithm overloads 
+
+        public static int EuclideanAlgorithm(params int[] numbers)
+        {
+            if (numbers.Length <= 1) throw new ArgumentException(nameof(numbers));
+            
+            if (numbers == null) throw new ArgumentNullException(nameof(numbers));
+            
+
+            int result = 0;
+
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                result = FindGCD(result, numbers[i]);
+            }
+
+            return result;
+        }
+
+        public static int EuclideanAlgorithm(int firstNumber, int secondNumber)
+        {
+            return FindGCD(firstNumber, secondNumber);
+        }
+
+        public static int EuclideanAlgorithm(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return FindGCD(FindGCD(firstNumber, secondNumber), thirdNumber);
+        }
+
+        #endregion
+
+        #region Public Binary Euclidean Algorithm overloads
+
+        public static int BinaryEuclideanAlgorithm(params int[] numbers)
+        {
+            if (numbers.Length <= 1) throw new ArgumentException(nameof(numbers));
+
+            if (numbers == null) throw new ArgumentNullException(nameof(numbers));
+
+
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length ; i++)
+            {
+                result = FindGCD(result, numbers[i]);
+            }
+
+            return result;
+        }
+
+        public static int BinaryEuclideanAlgorithm(int firstNumber, int secondNumber)
+        {
+            return BinaryFindGCD(firstNumber, secondNumber);
+        }
+
+        public static int BinaryEuclideanAlgorithm(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return BinaryFindGCD(FindGCD(firstNumber, secondNumber), thirdNumber);
+        }
+
+        #endregion
+
+        #region Private API
+        
+        private static int BinaryFindGCD(int firstNumber, int secondNumber)
+        {
+
+            if (firstNumber == 0) return secondNumber;
+            if (secondNumber == 0) return firstNumber;
+
+            NumbersRefactor(firstNumber, secondNumber, out firstNumber, out secondNumber);
+
+            int k = 1;
+
+            while (firstNumber != 0 && secondNumber != 0)
+            {
+                while (firstNumber % 2 != 1 && secondNumber % 2 != 1)
+                {
+                    firstNumber /= 2;
+                    secondNumber /= 2;
+                    k *= 2;
+                }
+
+                while (firstNumber % 2 != 1) firstNumber /= 2;
+                while (secondNumber % 2 != 1) secondNumber /= 2;
+
+
+                if (firstNumber >= secondNumber)
+
+                    firstNumber = firstNumber - secondNumber;
+                else
+                    secondNumber = secondNumber - firstNumber;
+
+            }
+
+            return secondNumber * k;
+        }
+
+        private static int FindGCD(int firstNumber, int secondNumber)
+        {
+
+            if (firstNumber == 0) return secondNumber;
+            if (secondNumber == 0) return firstNumber;
+
+            NumbersRefactor(firstNumber, secondNumber, out firstNumber, out secondNumber);
+
+            int Finder(int big, int small)
+            {
+                int rest = -1;
+                
+                while (big >= small)
+                {
+                    big = big - small;
+                    rest = big;
+                }
+
+                if (rest == 0)
+                {
+                    return small;
+                }
+
+                else
+                {
+                    big = small;
+                    small = rest;
+
+                    return Finder(big, small);
+                }
+
+            }
+
+            return Finder(firstNumber, secondNumber);
+
+        }
+        
+        private static void NumbersRefactor(int inFirst, int inSecond ,out int firstNumber, out int secondNumber)
+        {
+            if (inFirst < 0) inFirst *= -1;
+            if (inSecond < 0) inSecond *= -1;
+
+            if (inFirst < inSecond)
+            {
+                firstNumber = inSecond;
+                secondNumber = inFirst;
+                return;
+            }
+            else
+            {
+                firstNumber = inFirst;
+                secondNumber = inSecond;
+                return;
+            }
+
+        }
+        
+        #endregion
+
+    }
+}
---
+++ NET.W.2018.Lazuko.03-04/GCDTests/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using static GCDEuclideanAlgorithm.GCDFinder;
+
+namespace GCDTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public int EuclideanAlgorithm_ReceivesNull_ThrowArgumentNullException()
+        {
+            int[] numbers = null;
+
+            return EuclideanAlgorithm(numbers);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public int BinaryEuclideanAlgorithm_ReceivesNull_ThrowArgumentNullException()
+        {
+            int[] numbers = null;
+
+            return BinaryEuclideanAlgorithm(numbers);
+
+        }
+    }
+
+    [TestFixture]
+    public class NUnitTests
+    {
+
+        [TestCase(11, 33, 66, 0, 110, ExpectedResult = 11)]
+        [TestCase(11, 33, 66, 99, 110, ExpectedResult = 11)]
+
+        public int GCDEuclideanAlgorithm_ReceivesSetOfNumbers_ReturnsGCD(params int[] numbers)
+        {
+            return EuclideanAlgorithm(numbers);
+        }
+
+
+        [TestCase(1071, 462, ExpectedResult = 21)]
+        [TestCase(113, 0, ExpectedResult = 113)]
+        [TestCase(-21, 15, ExpectedResult = 3)]
+        [TestCase(37, 13, ExpectedResult = 1)]
+
+        public int GCDEuclideanAlgorithm_ReceiveTwoNumbers_ReturnsGCD(int first, int second)
+        {
+            return EuclideanAlgorithm(first, second);
+        }
+
+
+
+        [TestCase(11, 0, 0, ExpectedResult = 11)]
+        [TestCase(11, 12, 13, ExpectedResult = 1)]
+        [TestCase(11, 33, 66, ExpectedResult = 11)]
+
+        public int GCDEuclideanAlgorithm_ReceiveThreeNumbers_ReturnsGCD(int first, int second, int third)
+        {
+            return EuclideanAlgorithm(first, second, third);
+        }
+
+
+
+        [TestCase(11, 33, 66, 0, 110, ExpectedResult = 11)]
+        [TestCase(11, 33, 66, 99, 110, ExpectedResult = 11)]
+
+        public int BinaryEuclideanAlgorithm_ReceivesSetOfNumbers_ReturnsGCD(params int[] numbers)
+        {
+            return BinaryEuclideanAlgorithm(numbers);
+        }
+
+
+        [TestCase(1071, 462, ExpectedResult = 21)]
+        [TestCase(113, 0, ExpectedResult = 113)]
+        [TestCase(-21, 15, ExpectedResult = 3)]
+        [TestCase(37, 13, ExpectedResult = 1)]
+
+        public int BinaryEuclideanAlgorithm_ReceiveTwoNumbers_ReturnsGCD(int first, int second)
+        {
+            return BinaryEuclideanAlgorithm(first, second);
+        }
+
+
+
+        [TestCase(11, 0, 0, ExpectedResult = 11)]
+        [TestCase(11, 12, 13, ExpectedResult = 1)]
+        [TestCase(11, 33, 66, ExpectedResult = 11)]
+
+        public int BinaryEuclideanAlgorithm_ReceiveThreeNumbers_ReturnsGCD(int first, int second, int third)
+        {
+            return BinaryEuclideanAlgorithm(first, second, third);
+        }
+
+    }
+
+
+   
+}
