a3521ef3258d11d04edb10386ef7291ab74934b5
serafim <serafim.lazuko@gmail.com>
2018-10-18 17:53:11 +0300

    not completedgit status! but in the process

---
+++ NET.W.2018.Lazuko.03-04/StringExtension/Class1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension
+{
+    public static class StringExtension
+    {
+        public static string DoubleToStringConventer(double number)
+        {
+
+            return DoubleToBinary(number);
+        }
+
+        private static string DoubleToBinary(double number)
+        {
+            int sign = 0;
+            int exponenta = 0;
+
+            if (number < 0)
+            {
+                number *= -1;
+                sign = 1;
+            }
+
+
+            if (number < 1.0)
+                while (number < 1)
+                {
+                    number *= 10;
+                    exponenta -= 1;
+                }
+            else
+                while (number >= 10)
+                {
+                    number *= -10;
+                    exponenta += 1;
+                }
+
+
+            Tuple<int ,int[], int> normalizedForm = new Tuple<int, int[], int>(sign, DoubleNormilizedToBinary(number), exponenta);
+
+            
+           // string sd = string.Join("", BinaryToIEEEFormat(normalizedForm));
+
+            
+
+            
+
+            return string.Join("", BinaryToIEEEFormat(normalizedForm)); 
+        }
+
+        private static int[] BinaryToIEEEFormat(Tuple<int, int[], int> normalizedForm)
+        {
+            int[] order = IntToBinary(normalizedForm.Item3 + 1023);
+
+            int[] mantissa = normalizedForm.Item2;
+
+            int sign = normalizedForm.Item1;
+
+            int[] result = new int[64];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i == 0)
+                    result[i] = sign;
+
+                if (i > 0 && i <= 11)
+                    result[i] = order[i - 1];
+
+                if (i > 11)
+                    result[i] = mantissa[i - 12];
+            }
+            
+            return result;
+        }
+
+        private static int[] DoubleNormilizedToBinary(double number)
+        {
+            int[] binaryArray = new int[52];
+
+            binaryArray[0] = (int)number % 10;
+
+            number -= binaryArray[0];
+
+            for(int i = 1; i < binaryArray.Length; i++)
+            {
+                binaryArray[i] = (int) (2 * number) % 10;
+                number *= 2;
+
+                if (number >= 1) number -= number % 10;
+            }
+            return binaryArray;
+        }
+
+        private static int[] IntToBinary(int number)
+        {
+            int[] binaryArray = new int[11];
+
+            for(int i = 0; i < binaryArray.Length; i++)
+            {
+                binaryArray[i] = number % 2;
+                number = number / 2;
+            }
+
+            return binaryArray;
+        }
+    }
+}
