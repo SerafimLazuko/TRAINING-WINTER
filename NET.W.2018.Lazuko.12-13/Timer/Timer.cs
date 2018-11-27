ea5308c3023b524e1e81b8c48e9c3708bba49649
serafim <serafim.lazuko@gmail.com>
2018-11-28 01:12:11 +0300

    Timer

---
+++ NET.W.2018.Lazuko.12-13/Timer/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Timer
+{
+    /// <summary>
+    /// class Timer;
+    /// </summary>
+    public class Timer
+    {
+        public EventHandler<TimerInfoEventArgs> TimerOuted;
+
+        /// <summary>
+        /// Starts the timer. Then raises event.
+        /// </summary>
+        /// <param name="seconds">The seconds.</param>
+        private void StartTimer(int seconds)
+        {
+            Thread.Sleep(seconds / 10);
+
+            OnTimerOuted(new TimerInfoEventArgs());
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:TimerOuted" /> event.
+        /// </summary>
+        /// <param name="info">The <see cref="TimerInfoEventArgs"/> instance containing the event data.</param>
+        protected virtual void OnTimerOuted(TimerInfoEventArgs info)
+        {
+            TimerOuted?.Invoke(this, info);
+        }
+    }
+}
