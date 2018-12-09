180cf7bf85cded9ca73d4cf248bc5c6d2b1ca918
serafim <serafim.lazuko@gmail.com>
2018-12-09 16:07:07 +0300

    Task 1

---
+++ ExtTraining.Autumn.2018.3/No1.Solution/Conditions/DefaultConditions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No1.Solution
+{
+    public class DefaultConditions : IValidConditions
+    {
+        public string Conditions => @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{7,10}";
+    }
+}
---
+++ ExtTraining.Autumn.2018.3/No1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace No1.Solution
+{
+    public class PasswordCheckerService
+    {
+        public (bool, string) VerifyPassword(string password, IRepository repository, IValidConditions validConditions)
+        {
+            if (password == null)
+                throw new ArgumentException($"{password} is null arg");
+
+            if (repository == null)
+                throw new ArgumentException($"{repository} is null arg");
+
+            if (validConditions == null)
+                throw new ArgumentException($"{validConditions} is null arg");
+
+            if (password == string.Empty)
+                return (false, $"{password} is empty ");
+
+            string pattern = validConditions.Conditions;
+            
+            if(!Regex.IsMatch(password, pattern))
+                return (false, $"{password} is not valid!");
+
+            repository.Create(password);
+
+            return (true, "Password is Ok. User was created");
+        }
+    }
+}
